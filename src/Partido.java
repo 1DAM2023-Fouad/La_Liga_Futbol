dc69d7557ae13ca43e35d8a8e49586d249391229
Fouad <fouadaharchi1@gmail.com>
2023-11-26 18:55:52 +0100

    Creadas clases, plantilla pull request, readme y diagrama uml

---
+++ src/Partido.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Partido {
+    private String codigoPartido;
+    private String fechaPartido;
+    private String equipoLocal;
+    private String equipoVisitante;
+    private ArrayList<Equipo> equipos;
+    // (1,2) equivale a (1,n), porque en un partido solo hayy 2 equipos jugando
+
+    public String getCodigoPartido() {
+        return codigoPartido;
+    }
+
+    public void setCodigoPartido(String codigoPartido) {
+        this.codigoPartido = codigoPartido;
+    }
+
+    public String getFechaPartido() {
+        return fechaPartido;
+    }
+
+    public void setFechaPartido(String fechaPartido) {
+        this.fechaPartido = fechaPartido;
+    }
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
+
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public ArrayList<Equipo> getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(ArrayList<Equipo> equipos) {
+        this.equipos = equipos;
+    }
+}
